cf48dbae163d3a8b5d504317a439d98203e4cfa3
zer01one <aastoliarov02@gmail.com>
2024-05-15 11:57:29 +0300

    Add dynamic status

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,8 @@
 window.onload = () => {
+    Pincode.init(document.querySelectorAll('.pin-input'), (maxlength, value) => {
+        console.log('Pin code filled', maxlength, value);
 
-    Pincode.init(document.querySelectorAll('.pin-input'), (code) => {
-        console.log('Pin code filled', code);
+        $('#status-value strong').text(value);
+        $('#status-ready')[value.length === maxlength ? 'show' : 'hide']();
     });
 }
